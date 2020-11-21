1a58b280f4cf5c851a8c70f487bbd075d64bf151
Agustin Gonzales Hunzicker <agonzaleshunzicker@santafe.gov.ar>
2020-11-20 21:03:37 -0300

    dao

--- app/src/main/java/com/example/appequipos/Activity/Activity/MainActivity.java
+++ app/src/main/java/com/example/appequipos/Activity/Activity/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.appequipos.Activity.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.appequipos.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+            editText variable
+         */
         final EditText editName = findViewById(R.id.editTextNombreEquipo);
-        
+        /*
+            Spinner variable
+        */
+        final Spinner spinnerDeporte = findViewById(R.id.spinnerDeporte);
+
+        /*
+            Button variable
+        */
+        final Button btnRegistrar = findViewById(R.id.buttonRegistrarEquipo);
+        ArrayList deportes = new ArrayList<>();
+        ArrayAdapter spinnerAdapterYears = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, deportes);
+        spinnerDeporte.setAdapter(spinnerAdapterYears);
+
+        //btnRegistrar.setOnClickListener();
 
 
     }
